870fe338b8afbfdfc82b7adac1d350901f2a0db7
patymunoz <munpaty@gmail.com>
2025-10-07 17:54:34 -0600

    proyecto sphinx y readthedocs inicial

---
+++ docs/conf.py
@@ -0,0 +1,52 @@
+# docs/conf.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# --- Metadatos del proyecto ---
+project = 'Curso introducción a GTPs y su aplicación en la programación'
+author = 'MCD. Patricia Muñoz'
+release = '0.1'
+
+# --- Extensiones ---
+extensions = [
+    'myst_parser',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+# MyST
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath"
+]
+
+# Rutas y exclusiones
+templates_path = ['_templates']
+exclude_patterns = [
+    '_build',
+    '**/.ipynb_checkpoints',
+    'notebooks/*.ipynb',
+    '**/*.html',
+]
+
+# --- Tema ---
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/patymunoz/curso-gpt-programacion",
+    "repository_branch": "main",   
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_edit_page_button": False,
+    "use_issues_button": False,
+    "home_page_in_toc": True,
+}
+
+html_title = "Curso introducción a GTPs y su aplicación en la programación"
+html_logo = "_static/cerebro.png" 
+html_static_path = ['_static']
+
+# Reconocer archivos .md como Markdown
+source_suffix = {
+    '.md': 'markdown',
+}
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+# Curso de Machine Learning en Python
+
+## Presentación del curso
+
+## Objetivos del curso
+
+## Contenidos del curso
+
+Los contenidos del curso puedes encontrarlos en la parte izquierda de esta página, en la sección _"Contenidos"_.
+
+```{toctree}
+:hidden:
+:maxdepth: 1
+:caption: Contenidos
+
+guia/intro
+
+```
+
+## 🌐 Metodología
+
+El curso se dicta en modalidad presencial, combinando _exposiciones teóricas_ breves con _actividades prácticas_ guiadas en cada sesión. Se utilizará **Google Colab** como entorno de programación, lo que permite evitar instalaciones locales y facilita el acceso a todos los contenidos desde cualquier dispositivo.
+
+No se requiere experiencia avanzada en programación, aunque sí se recomienda contar con conocimientos básicos.
